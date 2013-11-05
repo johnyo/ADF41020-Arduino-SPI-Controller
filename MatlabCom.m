46d37da9ff11c2e2c6b36daf02111759be39310a
Joshua Miller <jmm146@duke.edu>
2013-11-05 11:43:45 -0500

    Allow for continuous user input

--- MatlabCom.m
+++ MatlabCom.m
@@ -2,17 +2,18 @@ clear all
 clc
  
 answer=1; % this is where we'll store the user's answer
+arduino=serial('/dev/ttyACM0','BaudRate',9600); % create serial communication object on port COM4
+ 
+% fopen(arduino); % initiate arduino communication
+%  fprintf(arduino, '%c',char(uint16('buzz')));
+% fclose(arduino); % end communication with arduino
+%  return;
 
-arduino=serial('/dev/ttyACM0','BaudRate',9600); % create serial communication object on port ttyACM0
- 
+while answer
+answer=input('Enter the command you want to send: ');
 fopen(arduino); % initiate arduino communication
- fprintf(arduino, '%c',char(uint16('Command1')));
+ fprintf(arduino, '%c',char(uint16(answer)));
 fclose(arduino); % end communication with arduino
- return;
+end
  
-% while answer
-%    answer=input('Enter the command you want to send: ');
-%    myans = uint16(answer);
-%    fprintf(arduino, '%c', char(myans));
-% end
- 
+fclose(arduino); % end communication with arduino
